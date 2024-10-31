b18efa7937fbfafd87ecf60005a922934b0e9bd8
Dany <dany-de-carvalho@outlook.fr>
2024-11-01 00:14:44 +0100

    ajout des dossiers et de pages pour le rooting et ainsi faire la nav bar

---
+++ src/app/blog/page.js
@@ -0,0 +1,9 @@
+"use server";
+
+export default async function BlogPage() {
+  return (
+    <main className="w-full h-screen flex items-center justify-center">
+      <h1 className="text-2xl">Blog Page</h1>
+    </main>
+  );
+}
---
+++ src/app/stockage/page.js
@@ -0,0 +1,9 @@
+"user server";
+
+export default async function StockagePage() {
+  return (
+    <main className="w-full h-screen flex items-center justify-center">
+      <h1 className="text-2xl">Stockage Page</h1>
+    </main>
+  );
+}
