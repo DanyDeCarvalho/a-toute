9a87ef0bf7b5b32cc2a2adb0093e886be7ecf8fc
DanyDeCarvalho <78216667+DanyDeCarvalho@users.noreply.github.com>
2024-11-01 00:52:14 +0100

    ajout navbar et footer a retravailler et ajout de class css sur le layout (#12)

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -4,7 +4,7 @@ import SwitchForms from "@/components/contact/SwitchForms";
 
 export default async function ContactPage() {
   return (
-    <main className="w-full h-screen flex items-center justify-center">
+    <main className="  w-full h-full flex flex-col items-center justify-center">
       <SwitchForms />
     </main>
   );
---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+"use client";
+import Link  from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="flex pl-12 pr-12 items-center justify-between w-dwh h-16 bg-neutral-900 text-white">
+      <Link href="/">
+        <p className="text-white">Home</p>
+      </Link>
+      <Link href="/blog">
+        <p className="text-white">Blog</p>
+      </Link>
+      <Link href="/services">
+        <p className="text-white">Services</p>
+      </Link>
+      <Link href="/espace-client">
+        <p className="text-white">Espace Client</p>
+      </Link>
+      <Link href="/faq">
+        <p className="text-white">FAQ</p>
+      </Link>
+      <Link href="/contact">
+        <p className="text-white">Contact</p>
+      </Link>
+    </nav>
+  );
+}
