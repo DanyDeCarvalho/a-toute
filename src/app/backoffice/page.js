c39e0a6b43ea66d4481d700152318361cfa2ce6c
Dany <dany-de-carvalho@outlook.fr>
2024-11-01 19:40:42 +0100

    commit complet

---
+++ src/app/backoffice/page.js
@@ -0,0 +1,9 @@
+"use server";
+
+export default async function BackOfficePage() {
+  return (
+    <div className="container mx-auto py-10">
+      <h1 className="text-2xl font-bold">Backoffice</h1>
+    </div>
+  );
+}
